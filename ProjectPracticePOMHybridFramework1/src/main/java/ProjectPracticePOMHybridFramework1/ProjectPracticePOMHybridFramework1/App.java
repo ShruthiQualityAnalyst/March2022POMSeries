db92f190254ec027e60a53e8e310a8d62ab673d7
ShruthiQualityAnalyst <kshruthi808@gmail.com>
2023-02-22 11:52:03 +0530

    m1

---
+++ ProjectPracticePOMHybridFramework1/src/main/java/ProjectPracticePOMHybridFramework1/ProjectPracticePOMHybridFramework1/App.java
@@ -0,0 +1,13 @@
+package ProjectPracticePOMHybridFramework1.ProjectPracticePOMHybridFramework1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
